58dddd3f40215b8856e2f14d818e765a4e6e61a3
keraemoe <weekera@gmail.com>
2023-04-11 00:38:37 +0600

    start

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import s from './Footer.modyle.scss'
+
+const Footer = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Footer;
---
+++ components/Layouts/Layouts.jsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+const Layouts = ({ childer }) => {
+    return (
+        <>
+            <Header />
+            {childer}
+            <Footer />
+        </>
+    );
+};
+
+export default Layouts;
