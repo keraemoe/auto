1ab6a131de4b90e4ac7d0df3aa808e1ce2d93215
Izatis <mekinovizat1@gmail.com>
2023-04-11 21:30:56 +0600

    add: header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,12 +1,49 @@
-﻿import React from 'react';
-import s from './Header.module.scss'
+﻿import React from "react";
+import s from "./Header.module.scss";
 
 const Header = () => {
-    return (
-        <div>
-
+  return (
+    <header className={s.header}>
+      <nav className={s.header_nav}>
+        <ul className={s.left_ul}>
+          <li>
+            <a href="#">Car List</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Contacts</a>
+          </li>
+        </ul>
+        <div className={s.logo_text}>
+          <img src="./logo.png" alt="logo" />
+          <img src="./logo_text.png" alt="logo_text" />
         </div>
-    );
+        {/* logo_text */}
+
+        <ul className={s.right_ul}>
+          <li>
+            <a href="#">+971 58 590 7875</a>
+          </li>
+
+          <select>
+            <option>Dubai</option>
+            <option>test1</option>
+            <option>test2</option>
+          </select>
+
+          <select>
+            <option>ENG</option>
+            <option>RUS</option>
+            <option>KGZ</option>
+          </select>
+        </ul>
+      </nav>
+      {/* nav */}
+    </header>
+    // header
+  );
 };
 
 export default Header;
