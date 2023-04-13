34ce1c924843516d99ab031dfe02d2ed85d92799
Izatis <125425878+Izatis@users.noreply.github.com>
2023-04-13 12:16:35 +0600

    Revert "add: NavBurgerMenu, BurgerMenu ,SocialNetwork, Logo"

--- components/CustomSelect/CustomSelect.jsx
+++ components/CustomSelect/CustomSelect.jsx
@@ -9,37 +9,35 @@ const CustomSelect = ({
   selectedOption,
   onClick,
 }) => {
-  
   return (
-    <div
-      className={
-        isOpen ? cn(s.customized_select, s.is_open) : s.customized_select
-      }
-    >
-      <span
-        className={s.select__placeholder}
-        onClick={() => setIsOpen(!isOpen)}
+      <div
+        className={
+          isOpen ? cn(s.customized_select, s.is_open) : s.customized_select
+        }
       >
-        {selectedOption}
-      </span>
-      {/* select__placeholder */}
+        <span
+          className={s.select__placeholder}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {selectedOption}
+        </span>
 
-      <ul className={s.select__list}>
-        {options.map((option) => {
-          return (
-             <li
-              className={s.select__list__item}
-              key={option.id}
-              onClick={() => onClick(option.label)}
-            >
-              {option.label}
-            </li>
-          );
-        })}
-      </ul>
-      {/* select__list */}
-    </div>
-    // customized_select, is_open
+        <ul className={s.select__list}>
+          {options.map((option) => {
+            return (
+              <>
+                <li
+                  className={s.select__list__item}
+                  onClick={() => onClick(option.label)}
+                >
+                  {option.label}
+                </li>
+                <hr />
+              </>
+            );
+          })}
+        </ul>
+      </div>
   );
 };
 
