8197a7cabcf9cbec5a534ba17261eb9afc6b73fd
Ba1tur <zbajtur@gmail.com>
2023-04-22 21:32:10 +0600

    finall

--- components/Discount/Discount.jsx
+++ components/Discount/Discount.jsx
@@ -1,9 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './Discount.module.scss'
 import Image from 'next/image';
 import left from '../../public/zyro-image.png'
+import axios from 'axios';
 
 const Discount = () => {
+	const [email, setEmail] = useState('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		try {
+			axios
+				.post('https://jsonplaceholder.typicode.com/posts', email)
+				.then((res) => {
+					console.log(res)
+					setEmail('')
+				})
+		} catch (err) {
+			console.log(err)
+		}
+	};
+
 	useEffect(() => {
 		const marqueeTrack = document.getElementById("marqueeTrack");
 		const spans = marqueeTrack.querySelectorAll("span");
@@ -18,7 +35,7 @@ const Discount = () => {
 			});
 		});
 	}, []);
-	
+
 	return (
 		<section className={s.discount_section}>
 			<div className={s.discount_section__background_top}></div>
@@ -29,31 +46,37 @@ const Discount = () => {
 				<Image className={s.discount_section__block_right__img} src={left} alt='right_img' />
 				<h1>Get a discount of up to 60%</h1>
 				<h2>Get the latest articles and business updates that you need to <br />  know, you’ll even get special recommendations weekly.</h2>
-				<div className={s.discount_section__block__footer}>
-					<input placeholder='Your email' type="email" />
+				<form onSubmit={handleSubmit} className={s.discount_section__block__footer}>
+					<input
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						minLength={3}
+						placeholder='Your email'
+						type="email"
+					/>
 					<button>Receive</button>
-				</div>
+				</form>
 			</div>
-				<div className={s.discount_section__block_background}>
-					<span>+</span>
-					<span>+</span>
-					<span>+</span>
-					<span>+</span>
-					<span>+</span>
-					<span>+</span>
-					<span>+</span>
-					<span>+</span>
-					<span>+</span>
-					<span style={{ color: 'red', visibility: 'hidden' }}>+</span>
-					<span>+</span>
-					<span>+</span>
-					<span style={{ color: '#33B7BC' }}>+</span>
-					<span style={{ color: 'red', visibility: 'hidden' }}>+</span>
-					<span style={{ color: 'red', visibility: 'hidden' }}>+</span>
-					<span style={{ color: '#33B7BC' }}>+</span>
-					<span>+</span>
-					<span>+</span>
-				</div>
+			<div className={s.discount_section__block_background}>
+				<span>+</span>
+				<span>+</span>
+				<span>+</span>
+				<span>+</span>
+				<span>+</span>
+				<span>+</span>
+				<span>+</span>
+				<span>+</span>
+				<span>+</span>
+				<span style={{ color: 'red', visibility: 'hidden' }}>+</span>
+				<span>+</span>
+				<span>+</span>
+				<span style={{ color: '#33B7BC' }}>+</span>
+				<span style={{ color: 'red', visibility: 'hidden' }}>+</span>
+				<span style={{ color: 'red', visibility: 'hidden' }}>+</span>
+				<span style={{ color: '#33B7BC' }}>+</span>
+				<span>+</span>
+				<span>+</span>
+			</div>
 			<div className={s.discount_section_brands}>
 				<div className={s.marquee}>
 					<div className={s.track} id='marqueeTrack'>
