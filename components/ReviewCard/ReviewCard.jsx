4488c7082b1fdb009e75ad0af21e4eff8c279391
akylxbet <democlbek@gmail.com>
2023-04-25 03:26:21 +1400

    added video in reviewblock and edited adaptive

--- components/ReviewCard/ReviewCard.jsx
+++ components/ReviewCard/ReviewCard.jsx
@@ -12,7 +12,7 @@ import slide from "../../assets/Merc.png";
 import slideTwo from "../../assets/Classic.png";
 import slideThree from "../../assets/Classic2.png";
 import slideFour from "../../assets/Audi.png";
-
+import Video from "yet-another-react-lightbox/plugins/video";
 const ReviewCard = () => {
   console.log(CarVideos);
 
@@ -36,44 +36,115 @@ const ReviewCard = () => {
         slidesPerView={4}
         className="myReviewSwiper"
         breakpoints={{
-          320:{
-
-            width:320, 
-            slidesPerView:2,
-            spaceBetween:10
+          320: {
+            width: 320,
+            slidesPerView: 2,
+            
           },
-          425:{
-            width:425,
-            slidesPerView:2,
-            spaceBetween:10
+          360: {
+            width: 360,
+            slidesPerView: 2,
+            
           },
+          375: {
+            width: 375,
+            slidesPerView: 2,
+            
+          },
+          425: {
+            width: 425,
+            slidesPerView: 2,
+            
+          },
+          455: {
+            width: 455,
+            slidesPerView: 2,
+            
+          },
+          485: {
+            width: 480,
+            slidesPerView: 2,
+            
+          },
+          500:{
+            width:500, slidesPerView:2,
+          }
+          ,
+          525:{
+            width:525, slidesPerView:2,
+          }
+          ,
+          545:{
+            width:545, slidesPerView:2,
+          }
+          ,
+          565:{
+            width:565, slidesPerView:2,
+          }
+          ,
+          585:{
+            width:585, slidesPerView:2,
+          }
+          ,
+          605:{
+            width:605, slidesPerView:2,
+          }
+          ,
+          625:{
+            width:625, slidesPerView:2,
+          }
+          ,
+          645:{
+            width:645, slidesPerView:2,
+          }
+          ,
           // when window width is >= 640px
-          640: {
-            width: 640,
-            slidesPerView: 3,
-            spaceBetween:10
+          660: {
+            width: 660,
+            slidesPerView: 2,
+            
           },
+          685:{
+            width:625, slidesPerView:2,
+          }
+          ,
+          706:{
+            width:706, slidesPerView:2,
+          }
+          ,
+          726:{
+            width:726, slidesPerView:2,
+          }
+          ,
+          746:{
+            width:746, slidesPerView:2,
+          }
+          ,
           // when window width is >= 768px
           768: {
             width: 768,
             slidesPerView: 3,
-            spaceBetween:10
+            
           },
-          1024:{
+          1024: {
             width: 1024,
-            slidesPerView: 4,
-            spaceBetween:10
+            slidesPerView: 3,
+            
           },
-          1440:{
+          1366: {
+            width: 1366,
+            slidesPerView: 3,
+            
+          },
+          1440: {
             width: 1440,
-            slidesPerView:4,
-            spaceBetween:10
+            slidesPerView: 4,
+            
           },
-          1920:{
+          1920: {
             width: 1920,
-            slidesPerView:4,
-            spaceBetween:10,
-          }
+            slidesPerView: 4,
+          },
         }}
       >
         {CarVideos.map((item) => {
@@ -85,13 +156,14 @@ const ReviewCard = () => {
             >
               <img src={item.link} alt="img" />
               <div className={s.overlay}>
-                <p>VISIT THE GALLERY</p>
+                <img src="/play.png" alt="play" />
               </div>
             </SwiperSlide>
           );
         })}
       </Swiper>
-      <Lightbox
+
+      {/* <Lightbox
         open={open}
         close={() => setOpen(false)}
         Keyboard
@@ -114,6 +186,26 @@ const ReviewCard = () => {
           pinchZoomDistanceFactor,
           scrollToZoom,
         }}
+      /> */}
+      <Lightbox
+        plugins={[Video]}
+        open={open}
+        close={() => setOpen(false)}
+        Keyboard
+        slides={[
+          {
+            type: "video",
+            width: 1920,
+            height: 1080,
+            poster: "../../assets/Merc.png",
+            sources: [
+              {
+                src: "https://player.vimeo.com/external/368320203.sd.mp4?s=38b1bac5d627b94fb902597643461ee5f233d00a&profile_id=164&oauth2_token_id=57447761",
+                type: "video/mp4",
+              },
+            ],
+          },
+        ]}
       />
     </div>
   );
--- constants/constants.js
+++ constants/constants.js
@@ -1,6 +1,6 @@
 ﻿export const CarVideos = [
     {
-      link: "https://images.pexels.com/photos/3166786/pexels-photo-3166786.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      link: "https://cdn.24.co.za/files/Cms/General/d/6913/bf23f3da106746649a20ce07242d19b3.png",
       id: '1'
     },
     {
