38e2078489e46bf8a842def734619947aca70ad4
Artur-Kogay <9571artur9571@gmail.com>
2023-04-25 01:41:17 +0600

    ok

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
-import AskUsAnything from '@/components/AskUsAnything/AskUsAnything'
+// import AskUsAnything from '@/components/AskUsAnything/AskUsAnything'
 import Hero from '@/components/Hero/Hero'
-import Revievv from '@/components/Revievv/Revievv'
+// import Revievv from '@/components/Revievv/Revievv'
 import Review from '@/components/Review/Review'
 import Head from 'next/head'
 
@@ -16,8 +16,8 @@ export default function Home() {
       </Head>
       <Hero />
       <Review />
-      <Revievv/>
-      <AskUsAnything />
+      {/* <Revievv/>
+      <AskUsAnything /> */}
     </>
   )
 }
