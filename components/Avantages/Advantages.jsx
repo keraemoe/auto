c248f682a8392d2f054afdf4810b8cfcff89c827
keraemoe <weekera@gmail.com>
2023-04-25 07:13:17 +0600

    OK

--- components/Avantages/Advantages.jsx
+++ components/Avantages/Advantages.jsx
@@ -5,19 +5,36 @@ import Image from 'next/image';
 const cars = [
     {
         id: 1,
-        img: '/adv.png'
+        img: '/adv.png',
+        tag: '40+ unique cars for rent from our fleet'
     },
     {
         id: 2,
-        img: '/adv.png'
+        img: '/adv.png',
+        tag: '40+ unique cars for rent from our fleet'
     },
     {
         id: 3,
-        img: '/adv.png'
+        img: '/adv.png',
+        tag: '40+ unique cars for rent from our fleet'
+    },
+]
+
+const cars2 = [
+    {
+        id: 1,
+        img: '/adv.png',
+        tag: '40+ unique cars for rent from our fleet'
     },
     {
-        id: 4,
-        img: '/adv.png'
+        id: 2,
+        img: '/adv.png',
+        tag: '40+ unique cars for rent from our fleet'
+    },
+    {
+        id: 3,
+        img: '/adv.png',
+        tag: '40+ unique cars for rent from our fleet'
     },
 ]
 
@@ -27,17 +44,26 @@ const Advantages = () => {
             <div className={s.advantages}>
                 <h1>Advantages</h1>
                 <div className={s.content}>
-                    {cars.map((item) => (
-                        <div className={s.up}>
-                            <Image
-                                className={s.img}
-                                width={639}
-                                height={459}
-                                src={item.img}
-                                alt='img'
-                            />
+                    <div className={s.up}>
+                        <div className={s.container}>
+                            {cars.map((car) => (
+                                <>
+                                    <Image width={639} height={459} src={car.img} alt="" />
+                                    <h1>{car.tag}</h1>
+                                </>
+                            ))}
                         </div>
-                    ))}
+                    </div>
+                    <div className={s.down}>
+                        <div className={s.container}>
+                            {cars2.map((car2) => (
+                                <>
+                                    <Image width={639} height={459} src={car2.img} alt="" />
+                                    <h1>{car2.tag}</h1>
+                                </>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div >
         </>
